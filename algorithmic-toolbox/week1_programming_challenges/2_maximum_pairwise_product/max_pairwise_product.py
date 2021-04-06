fcd77de3b09943c8627e2866481f1e2ac0d399fd
Harkishan Grewal <harkishangrewal@gmail.com>
2021-04-05 22:28:18 -0700

    Add max_pairwise_product solution in Python

--- algorithmic-toolbox/week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.py
+++ algorithmic-toolbox/week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.py
@@ -12,7 +12,23 @@ def max_pairwise_product(numbers):
     return max_product
 
 
+def max_pairwise_product_fast(numbers):
+    n = len(numbers)
+    
+    max_index1 = -1
+    for first in range(n):
+        if max_index1 == -1 or (numbers[first] > numbers[max_index1]):
+            max_index1 = first
+
+    max_index2 = -1
+    for second in range(n):
+        if (not second == max_index1) and ((max_index2 == -1) 
+            or (numbers[second] > numbers[max_index2])):
+            max_index2 = second
+    return numbers[max_index1] * numbers[max_index2]
+
+
 if __name__ == '__main__':
     input_n = int(input())
     input_numbers = [int(x) for x in input().split()]
-    print(max_pairwise_product(input_numbers))
+    print(max_pairwise_product_fast(input_numbers))
