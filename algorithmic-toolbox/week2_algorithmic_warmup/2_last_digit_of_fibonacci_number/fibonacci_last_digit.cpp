4c87613a4b61e39cab1fe9726e78c1569e6ea9e9
Harkishan Grewal <harkishangrewal@gmail.com>
2021-04-08 21:31:52 -0700

    Add Cpp & Python solution for fibonacci last digit problem

--- algorithmic-toolbox/week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/fibonacci_last_digit.cpp
+++ algorithmic-toolbox/week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/fibonacci_last_digit.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cassert>
 
 int get_fibonacci_last_digit_naive(int n) {
     if (n <= 1)
@@ -16,9 +17,37 @@ int get_fibonacci_last_digit_naive(int n) {
     return current % 10;
 }
 
+int get_fibonacci_last_digit_fast(int n) {
+    if (n <= 1)
+        return n;
+
+    int fibonacci_last[n];
+    fibonacci_last[0] = 0;
+    fibonacci_last[1] = 1;
+
+    for (int i = 2; i <= n; ++i) {
+        fibonacci_last[i] = (fibonacci_last[i - 1] + fibonacci_last[i - 2]) 
+                            % 10;
+    }
+
+    return fibonacci_last[n];
+}
+
+void test_solution() {
+    assert(get_fibonacci_last_digit_fast(3) == 2);
+    assert(get_fibonacci_last_digit_fast(331) == 9);
+    assert(get_fibonacci_last_digit_fast(327305) == 5);
+    for (int n = 0; n < 20; ++n) {
+        assert(get_fibonacci_last_digit_fast(n) == 
+                get_fibonacci_last_digit_naive(n));
+    }
+}
+
 int main() {
     int n;
     std::cin >> n;
-    int c = get_fibonacci_last_digit_naive(n);
+    //int c = get_fibonacci_last_digit_naive(n);
+    int c = get_fibonacci_last_digit_fast(n);
     std::cout << c << '\n';
-    }
+    //test_solution();
+}
--- algorithmic-toolbox/week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/fibonacci_last_digit.py
+++ algorithmic-toolbox/week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/fibonacci_last_digit.py
@@ -1,6 +1,4 @@
 # Uses python3
-import sys
-
 def get_fibonacci_last_digit_naive(n):
     if n <= 1:
         return n
@@ -13,7 +11,31 @@ def get_fibonacci_last_digit_naive(n):
 
     return current % 10
 
+
+def get_fibonacci_last_digit_fast(n):
+    if n <= 1:
+        return n
+
+    fibonacci_last = []
+    fibonacci_last.append(0)
+    fibonacci_last.append(1)
+
+    for i in range(2, n + 1):
+        fibonacci_last.append((fibonacci_last[i - 1] + 
+            fibonacci_last[i - 2]) % 10)
+
+    return fibonacci_last[n]
+
+
+def test_solution():
+    assert get_fibonacci_last_digit_fast(3) == 2
+    assert get_fibonacci_last_digit_fast(331) == 9
+    assert get_fibonacci_last_digit_fast(327305) == 5
+    for n in range(20):
+        assert get_fibonacci_last_digit_fast(n) == get_fibonacci_last_digit_naive(n)
+
+
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    n = int(input)
-    print(get_fibonacci_last_digit_naive(n))
+    n = int(input())
+    print(get_fibonacci_last_digit_fast(n))
+    #test_solution()
