a5eb3a7aaeccc17062969a768585f44e0a504a88
Harkishan Grewal <harkishangrewal@gmail.com>
2021-04-25 19:09:29 -0700

    Add solution for week 4

--- algorithmic-toolbox/week4_divide_and_conquer/2_majority_element/majority_element.cpp
+++ algorithmic-toolbox/week4_divide_and_conquer/2_majority_element/majority_element.cpp
@@ -5,10 +5,33 @@
 using std::vector;
 
 int get_majority_element(vector<int> &a, int left, int right) {
-  if (left == right) return -1;
-  if (left + 1 == right) return a[left];
-  //write your code here
-  return -1;
+    if (left == right) return -1;
+    if (left + 1 == right) return a[left];
+    
+    vector<int> tmp;
+    for (int i : a) {
+        if (!tmp.empty() && tmp[tmp.size() - 1] != i) {
+            tmp.pop_back();
+        }
+        else if (tmp.empty() || tmp[tmp.size() - 1] == i) {
+            tmp.push_back(i);
+        }
+    }
+    if (tmp.empty()) {
+        return -1;
+    }
+    int candidate = tmp[tmp.size() - 1];
+    tmp.pop_back();
+    int count = 0;
+    for (int x : a) {
+        if (x == candidate) {
+            ++count;
+        }
+    }
+    if (count > a.size() / 2) {
+        return candidate;
+    }
+    return -1;
 }
 
 int main() {
