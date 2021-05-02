eaf3349ed287af7f47b2891d44b82814610a5089
Harkishan Grewal <harkishangrewal@gmail.com>
2021-05-02 10:43:10 -0700

    Add Cpp solutions for week 5

--- algorithmic-toolbox/week5_dynamic_programming1/1_money_change_again/change_dp.cpp
+++ algorithmic-toolbox/week5_dynamic_programming1/1_money_change_again/change_dp.cpp
@@ -1,12 +1,30 @@
 #include <iostream>
+#include <limits>
+
+using namespace std;
 
 int get_change(int m) {
-  //write your code here
-  return m / 4;
+    int change[m + 1];
+    int c1, c3, c4;
+    change[0] = 0;
+    for (int i = 1; i <= m; ++i) {
+        c1 = c3 = c4 = numeric_limits<int>::max();
+        if (i - 1 >= 0) {
+            c1 = 1 + change[i - 1];
+            if (i - 3 >= 0) {
+                c3 = 1 + change[i - 3];
+                if (i - 4 >= 0) {
+                    c4 = 1 + change[i - 4];
+                }
+            }
+            change[i] = min(c1, min(c3, c4));
+        }
+    }
+    return change[m];
 }
 
 int main() {
-  int m;
-  std::cin >> m;
-  std::cout << get_change(m) << '\n';
+    int m;
+    std::cin >> m;
+    std::cout << get_change(m) << '\n';
 }
--- algorithmic-toolbox/week5_dynamic_programming1/3_edit_distance/edit_distance.cpp
+++ algorithmic-toolbox/week5_dynamic_programming1/3_edit_distance/edit_distance.cpp
@@ -4,14 +4,32 @@
 using std::string;
 
 int edit_distance(const string &str1, const string &str2) {
-  //write your code here
-  return 0;
+    int distance[str1.size() + 1][str2.size() + 1];
+    for (int i = 0; i <= str1.size(); ++i) {
+        distance[i][0] = i;
+    }
+    for (int i = 1; i <= str2.size(); ++i) {
+        distance[0][i] = i; 
+    }
+    for (int i = 1; i <= str1.size(); ++i) {
+        for (int j = 1; j <= str2.size(); ++j) {
+            if (str1[i - 1] != str2[j - 1]) {
+                distance[i][j] = std::min(1 + distance[i - 1][j], 
+                    std::min(1 + distance[i][j - 1], 
+                        1 + distance[i - 1][j - 1]));
+            }
+            else {
+                distance[i][j] = distance[i - 1][j - 1];
+            }
+        }
+    }
+    return distance[str1.size()][str2.size()];
 }
 
 int main() {
-  string str1;
-  string str2;
-  std::cin >> str1 >> str2;
-  std::cout << edit_distance(str1, str2) << std::endl;
-  return 0;
+    string str1;
+    string str2;
+    std::cin >> str1 >> str2;
+    std::cout << edit_distance(str1, str2) << std::endl;
+    return 0;
 }
